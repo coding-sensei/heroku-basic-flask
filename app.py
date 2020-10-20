8f427dfd8a522c1646b7d245e4d11949e259cd26
coding-sensei <investing.sensei.2019@gmail.com>
2020-10-20 04:33:43 +0100

    add rest route for testing

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 from datetime import datetime
+from flask_restful import Resource, Api
+
 app = Flask(__name__)
+api = Api(app)
+
+class personalCapital(Resource):
+    def get(self):
+        return {'about':'Personal Capital data'}
+
+    def post(self):
+        some_json = request.get_json()
+        return {'you sent': some_json}, 201
 
 @app.route('/')
 def homepage():
@@ -12,6 +23,8 @@ def homepage():
 
     <img src="http://loremflickr.com/600/400" />
     """.format(time=the_time)
+
+api.add_resource(personalCapital, '/test')
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
