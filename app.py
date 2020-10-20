7d815aeee52306cc78a6eb90ca2a8ea433e729ca
coding-sensei <investing.sensei.2019@gmail.com>
2020-10-20 04:49:08 +0100

    try heroku config variables

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
 from flask import Flask, request
 from datetime import datetime
 from flask_restful import Resource, Api
+from os import environ
 
 app = Flask(__name__)
 api = Api(app)
 
+
 class personalCapital(Resource):
     def get(self):
-        return {'about':'Personal Capital data'}
+        account = environ.get("ACCOUNT")
+        username = environ.get("USERNAME")
+        password = environ.get("PASSWORD")
+        return {
+            "about": "Personal Capital data",
+            "account": account,
+            "username": username,
+            "password": password,
+        }
 
     def post(self):
         some_json = request.get_json()
-        return {'you sent': some_json}, 201
+        return {"you sent": some_json}, 201
 
-@app.route('/')
+
+@app.route("/")
 def homepage():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
@@ -22,10 +33,12 @@ def homepage():
     <p>It is currently {time}.</p>
 
     <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
+    """.format(
+        time=the_time
+    )
 
-api.add_resource(personalCapital, '/test')
 
-if __name__ == '__main__':
+api.add_resource(personalCapital, "/test")
+
+if __name__ == "__main__":
     app.run(debug=True, use_reloader=True)
-
